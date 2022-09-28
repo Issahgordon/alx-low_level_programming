9a6a9934931a1a19a49f9ce0343fb018ea8c278b
Issahgordon <issahgordon3@gordon3@mail.com>
2022-09-28 08:42:16 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,50 @@
-#include "main.h"
+#include "holberton.h"
 
+int check_palindrome(char *s);
 /**
-* is_palindrome - returns 1 if a string is a palindrome and 0 if not
+* is_palindrome - Returns if a string is palindrom
+* @s: the string value to be checked
 *
-* @s: string
-* Return: int
+* Return: integer value
 */
 int is_palindrome(char *s)
 {
-int len = _strlen_recursion(s), i = 0;
-if (len == 0)
+if (*s == '0')
 return (1);
-return (palindrome_check(s, len - 1, i));
+return (check_palindrome(s));
 }
 /**
-* _strlen_recursion - returns the length of a string
+* check_palindrome - Check if a string is palindrome
+* @s: the string value to be checked
 *
-* @s: string
-* Return: int
+* Return: integer value
+*/
+int check_palindrome(char *s)
+{
+int l = _strlen_recursion(s) - 1;
+if (*s == s[l])
+{
+s++;
+l--;
+}
+else
+{
+return (0);
+}
+return (1);
+}
+/**
+* _strlen_recursion - Get the length of a string
+* @s: the string to get the length
+*
+* Return: the string length
 */
 int _strlen_recursion(char *s)
 {
-int len;
 if (*s == '\0')
-return (0);
-len = _strlen_recursion(s + 1);
-return (len + 1);
-}
-/**
-* palindrome_check - returns 1 if palindrome and 0 if not
-*
-* @s: string
-* @len: length of string
-* @i: counter variable
-* Return: int
-*/
-int palindrome_check(char *s, int len, int i)
 {
-if (len == i)
-return (1);
-else if (s[len] != s[i])
 return (0);
-if (i < len)
-return (palindrome_check(s, --len, ++i));
-return (1);
+}
+s++;
+return (_strlen_recursion(s) + 1);
 }
