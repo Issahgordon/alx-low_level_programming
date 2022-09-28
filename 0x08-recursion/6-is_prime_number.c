ef22c9de1bfd6eb7f39e21d1b0813e52f4838d36
Issahgordon <issahgordon3@gordon3@mail.com>
2022-09-28 09:18:26 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
 /**
-* is_prime_number - returns 1 if the input integer is a prime number,
-* otherwise returns 0
-*
-* @n: number
-* Return: int
-*/
-int is_prime_number(int n)
+  * _isitprime - looks for prime
+  * @n: the number
+  * @i: the number / 2
+  *
+  *Return: 1 if prime, 0 if not
+
+  */
+int _isitprime(int n, int i)
 {
-int i = 2;
-if (n <= 2)
-return (n == 2 ? 1 : 0);
-if (n % 2 == 0)
-return (0);
-return (is_prime(n, i));
+	if (i == 1)
+	{
+	return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+		return (0);
+		else
+		return (_isitprime(n, i - 1));
+	}
 }
 /**
-* is_prime - returns 1 if the input integer is a prime number,
-* otherwise returns 0
-*
-* @n: number
-* @i: dividend
-* Return: int
-*/
-int is_prime(int n, int i)
+ * is_prime_number - looks for prime
+ * @n: the number
+ * Return: 1 if orime, 0 if not
+ */
+int is_prime_number(int n)
 {
-if (n != i && n % i == 0)
-return (0);
-else if (n == i)
-return (1);
-return (is_prime(n, i + 1));
+	if (n < 2)
+	return (0);
+	if (n % 2 == 0)
+	return (0);
+	else
+	return (_isitprime(n, n / 2));
 }
