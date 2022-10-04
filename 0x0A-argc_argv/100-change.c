3c5f67ce36ca78fbe1ffd97dc9a54cace91c8ad0
Issahgordon <issahgordon3@gordon3@mail.com>
2022-10-03 20:56:47 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,65 +1,49 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
-/** 
-* main - prints the min num of coins to make change for an amount of money
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: int
- */
+/**
+* main - Prints the minimum number of coins to
+* make change for an amount of money.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: If the number of arguments is not exactly one - 1.
+* Otherwise - 0.
+*/
 int main(int argc, char *argv[])
 {
-	unsigned int count = 0;
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (atoi(argv[1]) < 0)
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
-	count = coin_count(count, atoi(argv[1]));
-	printf("%d\n", count);
-	return (0);
-}
-/**
- * coin_count - counts the min coins needed for change
- *
- * @count: int to count coins 
- * @num: argv[1] changed to int
- * Return: int
- */
-unsigned int coin_count(unsigned int count, int num)
+int cents, coins = 0;
+if (argc != 2)
 {
-	unsigned int sum = 0;
-	while (!(sum + 25 > (unsigned int)num))
-	{
-		sum += 25;
-		count++;
-	}
-	while (!(sum + 10 > (unsigned int)num))
-	{
-		sum += 10;
-		count++;
-	}
-	while (!(sum + 5 > (unsigned int)num))
-	{
-		sum += 5;
-		count++;
-	}
-	while (!(sum + 2 > (unsigned int)num))
-	{
-		sum += 2;
-		count++;
-	}
-	while (!(sum + 1 > (unsigned int)num))
-	{
-		sum += 1;
-		count++;
-	}
-	return (count);
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+while (cents > 0)
+{
+coins++;
+if ((cents - 25) >= 0)
+{
+cents -= 25;
+continue;
+}
+if ((cents - 10) >= 0)
+{
+cents -= 10;
+continue;
+}
+if ((cents - 5) >= 0)
+{
+cents -= 5;
+continue;
+}
+if ((cents - 2) >= 0)
+{
+cents -= 2;
+continue;
+}
+cents--;
+}
+printf("%d\n", coins);
+return (0);
 }
