7e8e61c767c7de61a6e9ccb9b01c5fc3010670b7
Issahgordon <issahgordon3@gordon3@mail.com>
2022-10-03 20:32:29 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,51 +1,37 @@
+
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* argstostr - a function that concatenates all the arguments of your program
-*@ac: count of args passed to the function
-*@av:array of arguments
+* argstostr - Concatenates all arguments of the program into a string;
+* arguments are separated by a new line in the string.
+* @ac: The number of arguments passed to the program.
+* @av: An array of pointers to the arguments.
 *
-*Return: pointer to the new string
+* Return: If ac == 0, av == NULL, or the function fails - NULL.
+* Otherwise - a pointer to the new string.
 */
 char *argstostr(int ac, char **av)
 {
-char *new_string = NULL;
-int k = 0, i = ac, j, sum = 0, temp = 0;
+char *str;
+int arg, byte, index, size = ac;
 if (ac == 0 || av == NULL)
 return (NULL);
-while (ac--)
-sum += (len(av[ac]) + 1);
-new_string = (char *) malloc(sum + 1);
-if (new_string != NULL)
+for (arg = 0; arg < ac; arg++)
 {
-while (k < i)
-{
-for (j = 0; av[k][j] != '\0'; j++)
-new_string[j + temp] = av[k][j];
-new_string[temp + j] = '\n';
-temp += (j + 1);
-k++;
+for (byte = 0; av[arg][byte]; byte++)
+size++;
 }
-new_string[temp] = '\0';
-}
-else
-{
+str = malloc(sizeof(char) * size + 1);
+if (str == NULL)
 return (NULL);
-}
-return (new_string);
-}
-/**
-*len - returns length of str
-*@str: string counted
-*Return: returns the length
-*/
-int len(char *str)
+index = 0;
+for (arg = 0; arg < ac; arg++)
 {
-int len = 0;
-if (str != NULL)
-{
-while (str[len])
-len++;
+for (byte = 0; av[arg][byte]; byte++)
+str[index++] = av[arg][byte];
+str[index++] = '\n';
 }
-return (len);
+str[size] = '\0';
+return (str);
 }
